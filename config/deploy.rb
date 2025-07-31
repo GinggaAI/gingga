975b68c8bfa3a835ec36a9bf2c4eda9453ab5ec8
Julian Vargas <julianvargasalvarez@gmail.com>
2025-08-01 00:02:43 +0200

    Runs rubocop

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:vlaguzman/gingga.git"
 set :deploy_to, "/var/www/#{fetch(:application)}"
 
 set :rvm_type, :user
-set :rvm_ruby_version, 'ruby-3.4.2' # or your actual version
+set :rvm_ruby_version, "ruby-3.4.2" # or your actual version
 
 set :linked_files, %w[config/database.yml config/master.key]
 set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets public/system storage]
